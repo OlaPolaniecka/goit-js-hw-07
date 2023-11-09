234f5e60ee6e5fff54f61185c820d7fea5134133
Ola Połaniecka <97olka@gmail.com>
2023-11-09 09:50:35 +0100

    JAVA7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+let modal;
+const closeLightBox = (event) => {
+  if (event.key === "Escape") {
+    modal.close();
+    gallery.removeEventListener("keyup", closeLightBox);
+  }
+};
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    modal = basicLightbox.create(`<img src=${event.target.dataset.source}>
+      `);
+    modal.show();
+    gallery.addEventListener("keyup", closeLightBox);
+  }
+});
+
+for (let item of galleryItems) {
+  let gallery__item = document.createElement("div");
+  let gallery__link = document.createElement("a");
+  let gallery__image = document.createElement("img");
+  gallery__item.classList.add("gallery__item");
+  gallery__link.classList.add("gallery__link");
+  gallery__image.classList.add("gallery__image");
+  gallery__link.setAttribute("href", item.original);
+  gallery__image.setAttribute("src", item.preview);
+  gallery__image.setAttribute("data-source", item.original);
+  gallery__image.setAttribute("alt", item.description);
+  gallery__link.append(gallery__image);
+  gallery__item.append(gallery__link);
+  gallery.append(gallery__item);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import simpleLightbox from "simplelightbox";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+let modal;
+const closeLightBox = (event) => {
+  if (event.key === "Escape") {
+    modal.close();
+    gallery.removeEventListener("keyup", closeLightBox);
+  }
+};
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains("gallery__image")) {
+    modal = new simpleLightbox(".gallery .gallery__item", {
+      captionsData: "alt",
+      captionDelay: 250,
+    });
+    modal.open();
+    gallery.addEventListener("keyup", closeLightBox);
+  }
+});
+
+for (let item of galleryItems) {
+  let gallery__item = document.createElement("a");
+  let gallery__image = document.createElement("img");
+  gallery__item.classList.add("gallery__item");
+  gallery__image.classList.add("gallery__image");
+  gallery__item.setAttribute("href", item.original);
+  gallery__image.setAttribute("src", item.preview);
+  gallery__image.setAttribute("data-source", item.original);
+  gallery__image.setAttribute("alt", item.description);
+  gallery__item.append(gallery__image);
+  gallery.append(gallery__item);
+}
