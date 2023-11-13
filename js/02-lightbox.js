1b1d6bae955d778a7891711bad8f7266d65c3ab1
Ola Połaniecka <97olka@gmail.com>
2023-11-13 10:41:28 +0100

    JAVA7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,10 +6,10 @@ console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
-let modal;
+let lightbox;
 const closeLightBox = (event) => {
   if (event.key === "Escape") {
-    modal.close();
+    lightbox.close();
     gallery.removeEventListener("keyup", closeLightBox);
   }
 };
@@ -18,24 +18,27 @@ gallery.addEventListener("click", (event) => {
   event.preventDefault();
 
   if (event.target.classList.contains("gallery__image")) {
-    modal = new simpleLightbox(".gallery .gallery__item", {
+    lightbox = new simpleLightbox(".gallery .gallery__item", {
       captionsData: "alt",
       captionDelay: 250,
     });
-    modal.open();
+    lightbox.open();
     gallery.addEventListener("keyup", closeLightBox);
   }
 });
 
 for (let item of galleryItems) {
-  let gallery__item = document.createElement("a");
+  let gallery__element = document.createElement("li");
+  let gallery__link = document.createElement("a");
   let gallery__image = document.createElement("img");
-  gallery__item.classList.add("gallery__item");
+  gallery__element.classList.add("gallery__element");
+  gallery__link.classList.add("gallery__item");
   gallery__image.classList.add("gallery__image");
-  gallery__item.setAttribute("href", item.original);
+  gallery__link.setAttribute("href", item.original);
   gallery__image.setAttribute("src", item.preview);
   gallery__image.setAttribute("data-source", item.original);
   gallery__image.setAttribute("alt", item.description);
-  gallery__item.append(gallery__image);
-  gallery.append(gallery__item);
+  gallery__link.append(gallery__image);
+  gallery__element.append(gallery__link);
+  gallery.append(gallery__element);
 }
